a1e7ace35e10548af96b860c0213b717091f5927
Asmita <brahmankarasmita99@gmail.com>
2023-04-04 16:50:50 +0530

    This is string1 updated  file

--- string1.py
+++ string1.py
@@ -1,3 +1,9 @@
 #This is string program
 s=("Hey good morning to everyone i'm asmita")
 print(s)
+
+
+s1=("hii this is python terminal and you log into github and run project on python")
+s2=len(s1)
+print(s2)
+
